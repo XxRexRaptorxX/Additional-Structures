01cb9b4dbca9675490ed3896f10c299f02716630
XxRexRaptorxX <Raptor-Simon@web.de>
2019-10-09 17:06:33 +0200

    v.2.3.0

--- src/main/java/xxrexraptorxx/additionalstructures/main/package-info.java
+++ src/main/java/xxrexraptorxx/additionalstructures/main/package-info.java
@@ -1,13 +1,6 @@
+package xxrexraptorxx.additionalstructures.main;
 
 /**---------------------------------------------------------------------------------*
  * @author XxRexRaptorxX (RexRaptor)
  * @projectPage https://minecraft.curseforge.com/projects/additional-structures
  **---------------------------------------------------------------------------------*/
-
-package xxrexraptorxx.additionalstructures.main;
-
-// * dont generate in flat worlds
-// hills to mountains
-// mod biome structures
-// silverfish cluster
-//stone circle ruin fix
