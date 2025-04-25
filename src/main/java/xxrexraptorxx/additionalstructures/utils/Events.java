0948037f123cae0a780f08202269fad2ed6a69c4
XxRexRaptorxX <Raptor-Simon@web.de>
2025-04-25 23:37:06 +0200

    mod repost info added

--- src/main/java/xxrexraptorxx/additionalstructures/utils/Events.java
+++ src/main/java/xxrexraptorxx/additionalstructures/utils/Events.java
@@ -25,19 +25,21 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.VersionChecker;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import xxrexraptorxx.additionalstructures.main.AdditionalStructures;
 import xxrexraptorxx.additionalstructures.main.References;
 
-import java.net.MalformedURLException;
+import java.io.IOException;
 import java.net.URI;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
-import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
 
 @EventBusSubscriber(modid = References.MODID, bus = EventBusSubscriber.Bus.GAME)
@@ -170,34 +172,89 @@ public class Events {
 
 
     /**
-     * Tests if a player is a supporter
+     * @author XxRexRaptorxX (RexRaptor)
      *
-     * @param url url to a file that contains the supporter names
-     * @param player ingame player
-     * @return true/false
+     * When entering a new MC installation for the first time, a message appears informing about the risks of mod reposts.
+     * Then generates a marker file in the configs folder with more details. Supports implementation in multiple mods.
+     *
+     * You are welcome to implement this method in your own mods!
      */
-    private static boolean SupporterCheck(URL url, Player player) {
+    @SubscribeEvent
+    public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
+        if (!(event.getEntity() instanceof ServerPlayer player)) return;
+
+        Path configDir = FMLPaths.CONFIGDIR.get();
+        Path marker   = configDir.resolve("#STOP_MOD_REPOSTS.txt");
+
         try {
-            Scanner scanner = new Scanner(url.openStream());
-            List<String> supporterList = scanner.tokens().toList();
+            if (Files.notExists(marker)) {
+                String fileContent = """
+                        Sites like 9minecraft.net, mc-mod.net, and many others, are known for reuploading mod files without permission from the authors. These sites will also contain a bunch of ads, to try to make money from mods they did not create.
+                        
+                        These sites will use various methods to appear higher in Google when you search for the mod name, so a lot of people will download mods from them instead of the proper place. If you were linked to this site, you're one of these people.
+                        
+                        FOR YOU, AS A PLAYER, THIS CAN MEAN ANY OF THE FOLLOWING:
+                        > Getting versions of the mods advertised for the wrong Minecraft versions, which will 100% crash when you load them.
+                        > Getting old, and broken, versions of the mods, possibly causing problems in your game.
+                        > Getting modified versions of the mods, which may contain malware and viruses.
+                        > Having your information stolen from malicious ads in the sites.
+                        > Taking money and views away from the official authors, which may cause them to stop making new mods.
 
-            for (String name: supporterList) {
-                //test if player is in supporter list
-                if (player.getName().getString().equals(name)) {
-                    return true;
-                }
+                        WHAT DO I DO NOW?
+                        The most important thing to do now is to make sure you stop visiting these sites, and get the mods from official sources. We also recommend you do the following:
+                        
+                        > Delete all the mods you've downloaded from these sites.
+                        > Install the StopModReposts plugin, which makes sure you never visit them again.
+                        > Run a virus/malware scan. We recommend MalwareBytes.
+                        > Check out the #StopModReposts campaign, that tries to put an end to these sites. (https://stopmodreposts.org/)
+                        > Spread the word. If you have any friends that use these sites, inform them to keep them safe.
+                        
+                        WHERE DO I GET MODS NOW?
+                        Here's a bunch of links to places where you can download official versions of mods, hosted by their real authors:
+                        
+                        > CurseForge, where most modders host their mods. If it exists, it's probably there.
+                        > Modrinth, a new hosting platform for mods that's also legit and more popular by the day.
+                        > OptiFine.net, the official OptiFine site.
+                        > Neoforged.net, which you need for any other Neoforge mods.
+                        > FabricMC.net, which you need for any other Fabric mods.
+                        > MinecraftForge Files, which you need for any other Forge mods.
+                        
+                        This doesn't mean other sites aren't legit. In general, the first place to look for a mod is CurseForge and Modrinth, so look there first.
+                        
+                        FAQ
+                        Q: What if I've never had problems before?
+                        > Just because you've never had problems with these sites before doesn't mean they're good. You should still avoid them for all the reasons listed above.
+                        
+                        Q: Is there a list of these sites I can check out?
+                        > Yes, however, due to these showing up all the time, it's possible to be incomplete. (https://github.com/StopModReposts/Illegal-Mod-Sites/blob/master/SITES.md)
+                        
+                        Q: Why can't you just take these sites down?
+                        > Unfortunately, these sites are often hosted in countries like Russia or Vietnam, where doing so isn't as feasible.
+                        
+                        Q: What if it says "Official Download" on the sites?
+                        > Sometimes they'll do that to trick you. If you're uncertain, you should verify with the StopModReposts list linked above.
+                        
+                        
+                        Credits: XxRexRaptorxX, Vazkii, StopModReposts campaign
+                    """;
+
+                Files.writeString(marker, fileContent, StandardCharsets.UTF_8);
+
+                player.sendSystemMessage(Component.literal("Important Information about mod reposts:\n").withStyle(ChatFormatting.UNDERLINE).withStyle(ChatFormatting.DARK_RED));
+                player.sendSystemMessage(Component.literal("Sites like 9minecraft.net, mc-mod.net, etc. are known for reuploading mod files without permissions. \nThese sites will also contain a bunch of ads, to try to make money from mods they did not create.\n").withStyle(ChatFormatting.RED));
+                player.sendSystemMessage(Component.literal("For you, this can mean any of the following:").withStyle(ChatFormatting.UNDERLINE).withStyle(ChatFormatting.RED));
+                player.sendSystemMessage(Component.literal("- Modified versions of mods, which may contain malware & viruses").withStyle(ChatFormatting.RED));
+                player.sendSystemMessage(Component.literal("- Having your information stolen from malicious ads").withStyle(ChatFormatting.RED));
+                player.sendSystemMessage(Component.literal("- Old and broken mod versions that can corrupt your world").withStyle(ChatFormatting.RED));
+                player.sendSystemMessage(Component.literal("- Taking money and views away from the real authors, which may cause them to stop making mods").withStyle(ChatFormatting.RED));
+
+                MutableComponent url = Component.literal(ChatFormatting.GOLD + "* Click here for more information *")
+                        .withStyle(style -> style.withClickEvent(new ClickEvent.OpenUrl(URI.create("https://vazkii.net/repost/"))));
+                player.sendSystemMessage(url);
             }
-
-            scanner.close();
-
-        } catch (MalformedURLException e) {
-            AdditionalStructures.LOGGER.error("Supporter list URL not found! >>{}", url);
-
-        } catch (Exception e) {
-            AdditionalStructures.LOGGER.error("An unexpected error occurred while checking supporter list", e);
+        } catch (IOException e) {
+            AdditionalStructures.LOGGER.error(e);
         }
-
-        return false;
     }
 
 }
